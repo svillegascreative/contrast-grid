c591608e44e8327479e5e362688c2db257a5302f
sylvia villegas <sylvia@svillegas.com>
2025-01-28 22:06:14 -0500

    create ColorGrid cmpt
    - add to index page in grid layout

---
+++ src/lib/ColorGrid.svelte
@@ -0,0 +1,28 @@
+<table>
+	<tbody>
+		<tr>
+			<td></td>
+			<th>Colour 1</th>
+			<th>Colour 2</th>
+			<th>Colour 3</th>
+		</tr>
+		<tr>
+			<th>Colour 1</th>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr>
+			<th>Colour 2</th>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr>
+			<th>Colour 3</th>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+	</tbody>
+</table>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import ColorForm from '$lib/ColorForm.svelte';
+	import ColorGrid from '$lib/ColorGrid.svelte';
 </script>
 
 <main>
@@ -8,4 +9,20 @@
 	<section aria-label="colour form">
 		<ColorForm />
 	</section>
+
+	<section aria-label="contrast grid">
+		<ColorGrid />
+	</section>
 </main>
+
+<style>
+	main {
+		display: grid;
+		grid-template-columns: 1fr 3fr;
+		gap: 2rem;
+	}
+
+	h1 {
+		grid-column: span 2;
+	}
+</style>
